7c37e18838a946962149a9abe1b6eb0e715338a2
Nadia Conroy <nadia.k.conroy@gmail.com>
2018-11-29 18:12:16 -0500

    Update unigramTranslate.py

--- unigramTranslate.py
+++ unigramTranslate.py
@@ -5,21 +5,27 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from fuzzywuzzy import fuzz
+import gensim
 from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 from nltk.corpus import stopwords
 from ppretty import ppretty
+
+
 import warnings
 warnings.filterwarnings("ignore")
 
 threshold = 80
 
-dict_path = "C:\develop\DataScienceMaster\Translate\data\dictionary.tsv"
+#dict_path = "C:\develop\DataScienceMaster\Translate\data\dictionary.tsv"
+dict_path = "C:\develop\DataScienceMaster\Translate\data\gangslang.tsv"
 unigram_pattern= r'^[a-zA-Z0-9\-\']+$'
 stops= set(stopwords.words('english'))
 file_path = "C:\develop\DataScienceMaster\Translate\data\input_text.txt"
-model = Word2Vec.load("saved_models/embeddings.model")
 
+model1 = Word2Vec.load("saved_models/embeddings4.model")
+model2 = gensim.models.KeyedVectors.load_word2vec_format('saved_models\GoogleNews-vectors-negative300.bin', binary=True, limit=500000)
+#model2 = Word2Vec.load("saved_models/embeddings5.model")
 
 class textLabel :
     def __init__(self, label, pos, length, thresh):
@@ -70,16 +76,6 @@ def remove_stopwords(content):
         filtered_sentence.append(w) 
   return(filtered_sentence)
          
-def read_dictionary_unigrams():
-    dict_terms ={}
-    with open(dict_path, "r") as f:
-         reader = csv.reader(f, delimiter='\t')
-         for key,value in reader:
-            dict_terms[key]=value;
-    f.close()
-     
-    return (get_dictionary_ngrams(dict_terms,1))
-
 def search_dictionary(targets, d, threshold):
    likely_matches = []
    for target in targets:
@@ -95,16 +91,12 @@ def get_sentences(content):
 def get_words(content):
    return (nltk.word_tokenize(content))
 
-def get_indicies(terms, s):
-   indicies =[]
-   for t in terms:
-      i = s.index(t)
-      indicies.append(i)
-   return (indicies)
+def get_index(term, s):
+   return (s.index(term))
 
 def get_dictionary_unigrams():
     dict_terms ={}
-    with open(dict_path, "r") as f:
+    with open(dict_path, "r", encoding="utf-8") as f:
          reader = csv.reader(f, delimiter='\t')
          for key,value in reader:
             dict_terms[key]=value;
@@ -121,59 +113,51 @@ def remove_sentence_word(term, sentence):
     if term in sentence: sentence.remove(term)
     return sentence
     
+
+def get_three_predictions(model, sentence):
+      cand1 = model.wv.doesnt_match(sentence)
+      sentence_v2 = remove_sentence_word(cand1,sentence)
+      cand2 = model.wv.doesnt_match(sentence_v2)
+      sentence_v3 = remove_sentence_word(cand2,sentence)
+      cand3 = model.wv.doesnt_match(sentence_v3)
+      return (cand1, cand2, cand3)
+
+
 content = read_file()
 
+
 for s in get_sentences(content):
-  candidates=[]
-  first_ver = remove_stopwords(s)
+  m1_candidates=[]
+  m2_candidates=[]
+  sent = remove_stopwords(s)
   try:
-    candidate1 = model.wv.doesnt_match(first_ver)
-    second_ver = remove_sentence_word(candidate1, first_ver)
-    candidate2 = model.wv.doesnt_match(second_ver)
-    candidates.append(candidate1)
-    candidates.append(candidate2)
+      pred1 = get_three_predictions(model1, sent)
+      pred2 = get_three_predictions(model2, sent)
+      common =list(set(pred1).intersection(pred2))
+      print (s)
+      print (common)
   except:
-    print ("no candidates found in a sentence")
-    continue;
-  matches = search_dictionary(candidates, get_dictionary_unigrams(), threshold)
-  print ("\nLikely matches for the sentence: ",s," With the candidiates ", candidates, " are the matches: ")
+      print ("no candidates found in a sentence")
+      continue;
+  if (common):
+      matches = search_dictionary(common, get_dictionary_unigrams(), threshold)
+  else:
+      print ("NO SLANG: The sentence [", s, "] had no predictions.")
+      continue;
   if bool(matches): 
     for m in matches:
-        print (ppretty(m))
-        #indicies=get_indicies(candidates,s)
-        #print ("The index values are")
-        #print (indicies)
+        indicies=get_index(m.candidate,s)
+        print ("SLANG: The sentence [",s,"] has the possible slang candidiate ", m.candidate, ". It matched with ", m.dict_term)
+        print ("its index values is", indicies )
         #add_label(s, candidate, index, confidence, length=1)
   else:
-    print ("nothing resembling that candidate found in dictionary")
-
-'''
-  if (match):
-  	
-
-
-
-textLabels = []
-tokenized = get_tokens(content)
-
-for thresh in thresholds:
-  token_idx = 0
-  for t in (tokenized):
-    token_idx=token_idx+1
-    for d in read_dictionary():
-      if (fuzz.ratio(t,d) > thresh):
-         tl1 = textLabel(t, token_idx, thresh)
-         textLabels.append (tl1)
-         
-for k in textLabels:
-  k.displayLabel()
-  k.displayPosition()
-  k.displayThreshold()
-  print ('======================================')
-'''
+     print ("nothing resembling that candidate found in dictionary")
 
 
 
 
 
-	    
+
+
+
+	   
