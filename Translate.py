13be2eb2585bd139f66ca8cbf027974ffbbe3212
Nadia Conroy <nadia.k.conroy@gmail.com>
2018-12-04 12:29:52 -0500

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -11,10 +11,11 @@ from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 from nltk.corpus import stopwords
 from ppretty import ppretty
-from tkinter import *  
+from Tkinter import *  
 from nltk.corpus import words
 from random import randint
 from HoverInfo import HoverInfo
+import operator
 
 
 import warnings
@@ -23,21 +24,27 @@ warnings.filterwarnings("ignore")
 ps = nltk.stem.SnowballStemmer('english')
 ngram_thresholds =[85]
 
-dict_path = "C:\develop\DataScienceMaster\Translate\data\dictionary.tsv"
+#dict_path = "C:\develop\DataScienceMaster\Translate\data\dictionary.tsv"
+dict_path = "dictionary.tsv"
 
-punc_pattern = r'[\S\t\r\n]+'
+
+symb_pattern = r'[\<\>\{\}\.\,\!\?\"\n$]+'
+punc_pattern = r'[\S\t\r]+'
 unigram_pattern= r'^[a-zA-Z0-9\-\']+$'
 bigram_pattern=r'^[a-zA-Z0-9\'\-]+\s[a-zA-Z0-9\'\-]+$'
 trigram_pattern=r'^[a-zA-Z0-9\']+\s[a-zA-Z0-9\']+\s[a-zA-Z0-9\']+$'
 quadgram_pattern=r'^[a-zA-Z0-9\']+\s[a-zA-Z0-9\']+\s[a-zA-Z0-9\']+\s[a-zA-Z0-9\']+$'
 stops= set(stopwords.words('english'))
 
-file_path = "data/input_text.txt"
+#file_path = "data/input_text.txt"
+file_path = "input_text.txt"
+
 colors=['sea green', 'maroon3', 'light salmon', 'slate blue', 'turquoise1','RoyalBlue1', 'coral', 'khaki1','ivory3','slate grey', 'yellow2', 'red3', 'purple']
 
 
-model1 = Word2Vec.load("saved_models/embeddings4.model")
-model2 = gensim.models.KeyedVectors.load_word2vec_format('saved_models\embeddingmodel6.bin', binary=True, limit=500000)
+#model1 = Word2Vec.load("saved_models/embeddings4.model")
+model2 = gensim.models.KeyedVectors.load_word2vec_format('saved_models/GoogleNews-vectors-negative300.bin', binary=True, limit=500000)
+model1 = gensim.models.KeyedVectors.load_word2vec_format('saved_models/GoogleNews-vectors-negative300.bin', binary=True, limit=500000)
 #model2 = Word2Vec.load("saved_models/embeddings5.model")
 
 class phraseLabel :
@@ -68,17 +75,17 @@ class Match(object):
 def get_threshold (term, l):
     threshold=80
     if l ==1 and len(term) <5:
-        threshold =89
+        threshold =87
     elif l == 1 and len(term) <6 :
-        threshold=88
+        threshold=86
     elif l ==1 and len(term) <7:
-        threshold=84
+        threshold=82
     elif l==1 and len(term) <8:
-        threshold=81
+        threshold=79
     return(threshold)
     
 def clean_term (term):
-    return (re.sub(r'[\.\,\!\?\"\n$]+','',term))
+    return (re.sub(symb_pattern,'',term))
 
 def read_file():
   with open(file_path, 'r') as ff:
@@ -127,7 +134,7 @@ def get_index(term, s):
 
 def get_dictionary_unigrams():
     dict_terms ={}
-    with open(dict_path, "r", encoding='utf-8', errors='ignore') as f:
+    with open(dict_path, "r") as f:
          reader = csv.reader(f, delimiter='\t')
          for key,value in reader:
             dict_terms[key]=value;
@@ -185,7 +192,7 @@ def check_samelabels (new_label, labels):
 
 def read_dictionary():
     dict_terms ={}
-    with open(dict_path, "r",  encoding='utf-8', errors='ignore') as f:
+    with open(dict_path, "r") as f:
          reader = csv.reader(f, delimiter='\t')
          for key,value in reader:
             dict_terms[key]=value;
@@ -207,7 +214,7 @@ def fuzzy_label(str_a, str_b, orig_str):
         for thresh in ngram_thresholds:
           if fuzz.ratio(str_a, test)> thresh:
             orig = orig_str.lower()
-            label = phraseLabel(str_a, orig.index(test), len(test), str_b, test, thresh)
+            label = phraseLabel(str_a, texts.lower().index(test), len(test), str_b, test, thresh)
             if check_samelabels(label, labels) == False:
                  labels.append(label)
         i=i+1
@@ -236,12 +243,12 @@ def collect_unigrams(texts):
     m2_candidates=[]
     sent = remove_stopwords(s)
     try:
-        pred1 = get_two_predictions(model1, sent)
-        pred2 = get_two_predictions(model2, sent)
-        common = list(set(pred1).intersection(pred2))
+        #pred1 = get_two_predictions(model1, sent)
+        #pred2 = get_two_predictions(model2, sent)
+        #common = list(set(pred1).intersection(pred2))
+        common =s[4]
         if (to_check):
             common.extend(to_check)
-            print (common)
     except Exception as e:
         print ("Error in running models: ",e)
         continue;
@@ -265,24 +272,60 @@ def collect_unigrams(texts):
 
 
 def collect_non_dict(texts):
+    terms= get_dictionary_unigrams()
+    i=0
     splitted = re.findall(punc_pattern,texts) 
-    for w in splitted:       
+    for w in splitted: 
+        i = i+1   
+        w=clean_term(w)   
         if is_English(w) == False:
-           print (w)
-           for s,t in get_dictionary_unigrams.items():
-               if w==s: print ('ok')                         
+           for key, val in terms.items():
+		threshold = get_threshold(key, 1)
+ 		if fuzz.ratio(key, w) > threshold:
+		  index = [m.start() for m in re.finditer(w, texts)]
+		  for ind in index:                  
+                     label = phraseLabel(key, ind, len(w), val, w, 85)                         
+                     if check_samelabels(label, all_labels) == False:
+	             	all_labels.append(label)
+    if (all_labels):
+	json_string = json.dumps([ob.__dict__ for ob in all_labels])
+        return(json_string)
+    else:
+      return ("No non English words were found in the dictionary ")
 
 
 texts= read_file()
 dict_grams = read_dictionary()
 all_labels = []
 
-collect_non_dict(texts)
-#all_found_ngrams =  (collect_ngrams(texts))
-#all_found_unigrams = (collect_unigrams(texts))
 
-'''
+all_found_ngrams = (collect_ngrams(texts))
+all_found_unigrams = (collect_unigrams(texts))
+all_found_noneng = (collect_non_dict(texts))
+
+
+
+
+all_labels.sort(key=operator.attrgetter("pos"),reverse=False)
+
+
+def click(self, event):
+    # get the index of the mouse click
+    index = self.MT.index("@%s,%s" % (event.x, event.y))
+
+        # get the indices of all "adj" tags
+    tag_indices = list(self.MT.tag_ranges('adj'))
+
+        # iterate them pairwise (start and end index)
+    for start, end in zip(tag_indices[0::2], tag_indices[1::2]):
+        # check if the tag matches the mouse click index
+        if self.MT.compare(start, '<=', index) and self.MT.compare(index, '<', end):
+            # return string between tag start and end
+            return (start, end, self.MT.get(start, end))
+
+
 if all_labels:
+   all_lab = sorted(all_labels, key=lambda l: l.pos, reverse=True)
    if (all_found_ngrams !='There were no known ngrams in the source text.'):
       print ("The ngrams json is ", json.loads(all_found_ngrams))
    if (all_found_unigrams!='There were no unigrams in the source text.'):
@@ -292,22 +335,24 @@ if all_labels:
    text = Text(window, width=100, height=300)
    text.insert('1.0', texts)
    text.pack(side="left") 
-   for l in all_labels:
+   for l in all_lab:
       newcolor = colors[randint(0,12)]
       start ="1.0+"+str(l.pos)+"chars"
       duration = l.pos+l.length
       end = "1.0+"+str(duration)+"chars"
-      
       text.tag_configure("highlight", background=newcolor)
       text.tag_add("highlight",start, end)
-      bu1=Button(window, text='TERM: <'+str(l.input_term)+'> at TEXT POSITION: <'+str(l.pos)+'>', width=10, bg=newcolor, fg='black')
+      bu1=Button(window, text='TERM: <'+str(l.input_term)+'> at POSITION: <'+str(l.pos)+'>', width=10, bg=newcolor, fg='black')
       lstring = 'DICTIONARY: '+str(l.dict_term)+'\nCONFIDENCE: '+str(l.thresh)+'\nDEFINITION: '+str(l.defin) 
       bu1.submitButton =  HoverInfo(bu1, lstring)  
       bu1.pack(fill=X, pady=7, padx=2)
-  
    window.mainloop()
 else:
    print (all_found_ngrams+'\n')
    print (all_found_unigrams+'\n')
-'''
+
+
+
+
+
 
